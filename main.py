e4d35dceb9174058c16e32bd192d19731f29fc6b
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-10-17 22:53:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+
+class Core(object):
+    def __init__(self):
+        self.interface = input("wlan0 or wlan1 >> ")
+        self.monitor_mode()
+
+    def __str__(self):
+        pass
+
+    def monitor_mode(self):
+        if self.interface:
+            print("Make sure!")
+            print(os.system("iwconfig | grep 'Nickname'"))
+            self.check = input("y/n >> ")
+            if self.check != "y":
+                self.interface = input("[CHANGE] wlan0 or wlan1 >> ")
+                os.system(f"sudo airmon-ng start {self.interface}")
+                self.beacon()
+            else:
+                os.system(f"sudo airmon-ng start {self.interface}")
+                self.beacon()
+        else:
+            print("Error! Try again.")
+
+    def beacon(self):
+        os.system(f"sudo mdk3 {self.interface} b -c 1 -f nets.lst -g -t -m -a -s 9999999")
+
+while True:
+    run = Core()
